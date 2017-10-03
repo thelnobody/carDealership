5504b07b3ba10b9088ceb49af724e46c6ed7a154
andreviallon <viallon.andre@gmail.com>
2017-10-03 15:38:39 +0200

    Upadate Ajax loop

--- js/script.js
+++ js/script.js
@@ -4,25 +4,26 @@ $(document).ready(function () {
 		url: "car.xml",
 		dataType: "xml",
 		success: function(response) {
-			$(response).find('cars').each(function(){
+			$(response).find('car').each(function(){
 				let brand = $(this).find("brand").text();
+				let model = $(this).find("model").text();
 				let price = $(this).find("price").text();
 				let description = $(this).find("desc").text();
 				let img = $(this).find("image").attr("file");
 				let card = `
-				<div class="card" >
-					<div class="cardTitle" style="background: url("img/${img}")">
-						<h2 class="cardh2">${brand}</h2>
+				<div class="card">
+					<div class="cardTitle" style="background: url('${img}') center;">
+						<h2 class="cardh2">${brand} - ${model}</h2>
 						<h2 class="price">${price} DDK</h2>
 					</div>
 					<div class="cardDesc">
-					<p>${description}</p>
+						<p>${description}</p>
+					</div>
 					<div class="cardReadMore">
-						<button class="">Se mere</button>
+						<button class="btn">Se mere</button>
 					</div>
 				</div>
 				`;
-				console.log(img);
 				$(card).appendTo("#wrapper");
 			});
 		}
